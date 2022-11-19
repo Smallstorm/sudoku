1d165e8155620ec16695808e13b17994fec46fd5
Анна Махова <small_storm@mail.ru>
2022-11-19 18:45:49 +0300

    new start

--- sudoku/sudoku.py
+++ sudoku/sudoku.py
@@ -17,29 +17,29 @@ print('\n')
 
 print(shiftRow,'\n')
 
-i = 0
-while i < 5:                    #shifting rows in the main grid
-    buf = grid[shiftRow[i]]
-    grid[shiftRow[i]] = grid[shiftRow[-i]]
-    grid[shiftRow[-i]] = buf
-    i += 1
+#i = 0
+#while i < 5:                    #shifting rows in the main grid
+#    buf = grid[shiftRow[i]]
+#    grid[shiftRow[i]] = grid[shiftRow[-i]]
+#    grid[shiftRow[-i]] = buf
+#    i += 1
 
-print('\t shifting rows in the main grid \n')
-for i in range(0,9): print(grid[i])
-print('\n')
+#print('\t shifting rows in the main grid \n')
+#for i in range(0,9): print(grid[i])
+#print('\n')
 
-trGrid = np.array(grid)         # transpose main grid
-trGrid = trGrid.transpose()
-for i in range(0,9): print(trGrid[i])
-print('\n')
+#trGrid = np.array(grid)         # transpose main grid
+#trGrid = trGrid.transpose()
+#for i in range(0,9): print(trGrid[i])
+#print('\n')
 
-i = 0
-while i < 5:                    #shifting rows(cols) in the main grid
-    buf = trGrid[shiftRow[i]]
-    trGrid[shiftRow[i]] = trGrid[shiftRow[-i]]
-    trGrid[shiftRow[-i]] = buf
-    i += 1
+#i = 0
+#while i < 5:                    #shifting rows(cols) in the main grid
+#    buf = trGrid[shiftRow[i]]
+#    trGrid[shiftRow[i]] = trGrid[shiftRow[-i]]
+#    trGrid[shiftRow[-i]] = buf
+#    i += 1
 
-print('\t shifting rows(cols) in the main grid \n')
-for i in range(0,9): print(trGrid[i])
-print('\n')
+#print('\t shifting rows(cols) in the main grid \n')
+#for i in range(0,9): print(trGrid[i])
+#print('\n')
