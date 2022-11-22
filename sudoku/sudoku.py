3cb27f51dad5fc4ef1e80d22b6b6ad8c4987aaf3
Анна Махова <small_storm@mail.ru>
2022-11-22 18:16:53 +0300

    + segm shift

--- sudoku/sudoku.py
+++ sudoku/sudoku.py
@@ -5,9 +5,7 @@ defRow = [1,2,3,4,5,6,7,8,9]
 shiftRow = list(range(0,9,3))
 grid = list()
 
-#random.shuffle(shiftRow)                   # + random (optional)
-
-                                            # shifting elements in the row -->3<--
+# shifting elements in the row -->3<--
 print('\t shifting elements in the row --> forming base grid\n')
 
 j = 0                                           
@@ -19,47 +17,62 @@ while j < 3:
         i += 1
     j += 1
 
-
+print('base grid:\n')
 for i in range(0,9): print(grid[i])
 print('\n')
 
-                                                        #shifting rows(cols) in the segment
+######################################################################################
+# function def
+######################################################################################  
 
-def shiftRowsInSeg(grid, shiftRow):
+#shifting rows(cols) in the segment
+def shiftRowsInSeg(Grid, shiftRow):
     shiftGrid = [0,1,2]
     random.shuffle(shiftGrid)
     j = 0
     while j < 3:
         i = 0
         while i < 3:                  
-            buf = grid[shiftRow[j] + shiftGrid[i]]        
-            grid[shiftRow[j] + shiftGrid[i]] = grid[shiftRow[j] + shiftGrid[i-1]]
-            grid[shiftRow[j] + shiftGrid[i-1]] = buf
+            buf = Grid[shiftRow[j] + shiftGrid[i]]        
+            Grid[shiftRow[j] + shiftGrid[i]] = Grid[shiftRow[j] + shiftGrid[i-1]]
+            Grid[shiftRow[j] + shiftGrid[i-1]] = buf
             i += 1
         j += 1
+    return(Grid)
+
+#shifting segments
+def shiftSegmsInGrid(Grid,shiftRow):
+    shiftGrid = [0,1,2]
+    i = 0
+    while i < 3:                  
+        buf0 = Grid[shiftRow[i]]
+        buf1 = Grid[shiftRow[i] + 1]        
+        buf2 = Grid[shiftRow[i] + 2]        
+        Grid[shiftRow[i] + 0] = Grid[shiftRow[i] + 0 - 3]
+        Grid[shiftRow[i] + 1] = Grid[shiftRow[i] + 1 - 3]
+        Grid[shiftRow[i] + 2] = Grid[shiftRow[i] + 2 - 3]
+        Grid[shiftRow[i] + 0 - 3] = buf0
+        Grid[shiftRow[i] + 1 - 3] = buf1
+        Grid[shiftRow[i] + 2 - 3] = buf2
+        i += 1
+        return(Grid)
+
+# transpose main grid         
+def grid_transpose(Grid):                                       
+    grid = [[Grid[j][i] for j in range(len(Grid))] for i in range(len(Grid[0]))]
     return(grid)
 
-         
-def grid_transpose(TrGrid):                                       # transpose main grid
-    grid = [[TrGrid[j][i] for j in range(len(TrGrid))] for i in range(len(TrGrid[0]))]
-    return(grid)
 
+################################################################################################
+# function cols
+################################################################################################
 
+mix_func = ['grid_transpose(grid)', 'shiftRowsInSeg(grid, shiftRow)', 'shiftSegmsInGrid(grid,shiftRow)']
 
-grid = grid_transpose(grid)
-for i in range(0,9): print(grid[i])
-print('\n')
-
-
-j = 0
-while j < 3:
-    shiftRowsInSeg(grid, shiftRow)   
-    for i in range(0,9): print(grid[i])
-    print('\n')
-    j += 1
-
-grid = grid_transpose(grid)
-for i in range(0,9): print(grid[i])
-print('\n')
-
+for i in range(1,20):
+    id_func = random.randrange(0,len(mix_func),1)
+    grid = eval(mix_func[id_func])
+    print(id_func,'\n')
+print('\n')    
+for j in range(0,9): print(grid[j])
 
